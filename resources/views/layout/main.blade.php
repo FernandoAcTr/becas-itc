9adfc1fe328f35c90ea9e4f76e35be2508b38ae1
Fernando Acosta <Fernando Acosta>
2021-06-11 13:04:10 -0500

    arreglar problema de navegacion. Se uso el helper url de laravel

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -23,10 +23,10 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="/">Registrarse</a>
+                    <a class="nav-link" href="{{url('/')}}">Registrarse</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/solicitudes">Solicitudes</a>
+                    <a class="nav-link" href="{{url('/solicitudes')}}">Solicitudes</a>
                 </li>
             </ul>
         </div>
